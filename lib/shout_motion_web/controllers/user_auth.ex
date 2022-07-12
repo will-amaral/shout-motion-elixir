68709bc91d2f89438ba33ef84c7be27bccd22f60
Will Amaral <will@amaral.dev>
2022-07-11 23:29:08 -0300

    feat(styles): add base styling

--- lib/shout_motion_web/controllers/user_auth.ex
+++ lib/shout_motion_web/controllers/user_auth.ex
@@ -81,7 +81,7 @@ defmodule ShoutMotionWeb.UserAuth do
     conn
     |> renew_session()
     |> delete_resp_cookie(@remember_me_cookie)
-    |> redirect(to: "/")
+    |> redirect(to: "/login")
   end
 
   @doc """
