2dac48371ec868f157a085885272b6c53290afb0
Will Amaral <will@amaral.dev>
2022-07-11 23:29:08 -0300

    feat(styles): add base styling

--- lib/shout_motion_web/router.ex
+++ lib/shout_motion_web/router.ex
@@ -18,23 +18,39 @@ defmodule ShoutMotionWeb.Router do
   end
 
   scope "/", ShoutMotionWeb do
-    pipe_through :browser
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
 
-    get "/", PageController, :index
+    get "/criar-conta", UserRegistrationController, :new
+    post "/criar-conta", UserRegistrationController, :create
+    get "/login", UserSessionController, :new
+    post "/login", UserSessionController, :create
+    get "/resetar-senha", UserResetPasswordController, :new
+    post "/resetar-senha", UserResetPasswordController, :create
+    get "/resetar-senha/:token", UserResetPasswordController, :edit
+    put "/resetar-senha/:token", UserResetPasswordController, :update
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", ShoutMotionWeb do
-  #   pipe_through :api
-  # end
+  scope "/", ShoutMotionWeb do
+    pipe_through [:browser, :require_authenticated_user]
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
+    get "/", PageController, :index
+    get "/configuracoes", UserSettingsController, :edit
+    put "/configuracoes", UserSettingsController, :update
+    get "/configuracoes/confirmar-email/:token", UserSettingsController, :confirm_email
+  end
+
+  scope "/", ShoutMotionWeb do
+    pipe_through [:browser]
+
+    delete "/logout", UserSessionController, :delete
+    get "/confirmar", UserConfirmationController, :new
+    post "/confirmar", UserConfirmationController, :create
+    get "/confirmar/:token", UserConfirmationController, :edit
+    post "/confirmar/:token", UserConfirmationController, :update
+  end
+
+  # Development routes
+
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
@@ -45,48 +61,11 @@ defmodule ShoutMotionWeb.Router do
     end
   end
 
-  # Enables the Swoosh mailbox preview in development.
-  #
-  # Note that preview only shows emails that were sent by the same
-  # node running the Phoenix server.
   if Mix.env() == :dev do
     scope "/dev" do
       pipe_through :browser
 
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
-  end
-
-  ## Authentication routes
-
-  scope "/", ShoutMotionWeb do
-    pipe_through [:browser, :redirect_if_user_is_authenticated]
-
-    get "/users/register", UserRegistrationController, :new
-    post "/users/register", UserRegistrationController, :create
-    get "/users/log_in", UserSessionController, :new
-    post "/users/log_in", UserSessionController, :create
-    get "/users/reset_password", UserResetPasswordController, :new
-    post "/users/reset_password", UserResetPasswordController, :create
-    get "/users/reset_password/:token", UserResetPasswordController, :edit
-    put "/users/reset_password/:token", UserResetPasswordController, :update
-  end
-
-  scope "/", ShoutMotionWeb do
-    pipe_through [:browser, :require_authenticated_user]
-
-    get "/users/settings", UserSettingsController, :edit
-    put "/users/settings", UserSettingsController, :update
-    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
-  end
-
-  scope "/", ShoutMotionWeb do
-    pipe_through [:browser]
-
-    delete "/users/log_out", UserSessionController, :delete
-    get "/users/confirm", UserConfirmationController, :new
-    post "/users/confirm", UserConfirmationController, :create
-    get "/users/confirm/:token", UserConfirmationController, :edit
-    post "/users/confirm/:token", UserConfirmationController, :update
   end
 end
